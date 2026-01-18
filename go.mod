5b5e64db4f6715baa3ae6fc99e9928db568440fb
strjkc <strahinjaa.va@gmail.com>
2026-01-18 18:06:11 +0100

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/strjkc/chirpy
+
+go 1.24.3
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import "net/http"
+
+func readinessHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "text/plain")
+	w.Write([]byte("OK"))
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", readinessHandler)
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
